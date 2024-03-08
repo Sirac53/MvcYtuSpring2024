888c7c6a797c13234b8d83a3fb2dd748207770cc
Sirac53 <omersiracislamoglu@gmail.com>
2024-03-08 16:45:00 +0300

    FakeDatamade

---
+++ W02/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using W02.Models;
+
+namespace W02.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        public StudentsController()
+        {
+            SchoolDB.InitializeDb(50);
+        }
+
+        public IActionResult Index()
+        {
+            var students = SchoolDB.Students;
+            return View();
+        }
+    }
+}
---
+++ W02/Models/SchoolDB.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+
+namespace W02.Models
+{
+    public static class SchoolDB
+    {
+
+        public static List<Student> Students = new List<Student>();
+       
+        public static void InitializeDb(int number)
+        {
+
+            if (Students.Count == 0)
+            {
+                for (int i = 0; i <= number; i++)
+                {
+                    var student = new Faker<Student>()
+                        .RuleFor(s => s.Id, f => f.IndexFaker + 1)
+                        .RuleFor(s => s.Name, f => f.Name.FullName())
+                        .RuleFor(s => s.Email, f => f.Internet.Email())
+                        .RuleFor(s => s.Age, f => f.Random.Int(18, 38));
+
+                    Students.Add(student);
+                }
+            }
+        
+        }
+
+
+    }
+}
